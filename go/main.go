ad8a083c83dace6da7ba8ebc5bea89434770fd70
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-21 18:26:53 +0900

    goodbye apm

--- go/main.go
+++ go/main.go
@@ -208,7 +208,7 @@ func NewMySQLConnectionEnv() *MySQLConnectionEnv {
 
 func (mc *MySQLConnectionEnv) ConnectDB() (*sqlx.DB, error) {
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true&loc=Asia%%2FTokyo&interpolateParams=true", mc.User, mc.Password, mc.Host, mc.Port, mc.DBName)
-	return sqlx.Open("nrmysql", dsn)
+	return sqlx.Open("mysql", dsn)
 }
 
 func init() {
