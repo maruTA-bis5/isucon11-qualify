3245ada732ff4e9dd9dddae4c84438167b5b0a1a
Takayuki Maruyama <bis5.wsys@gmail.com>
2021-08-21 13:15:12 +0900

    restart.sh: build app

--- restart.sh
+++ restart.sh
@@ -4,3 +4,9 @@ sudo systemctl stop nginx
 sudo rm -f /var/log/nginx/access.log
 sudo systemctl start nginx
 
+
+sudo systemctl stop isucondition.go
+cd ~isucon/repo/go
+PATH=~isucon/local/go/bin:$PATH make
+sudo systemctl start isucondition.go
+
